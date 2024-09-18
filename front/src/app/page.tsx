d9468aa53a23e3a35e2c5ced07196b6aee9c9fd1
elias salazar <eliassalazar@iMac-de-elias.local>
2024-09-17 19:35:15 -0500

    se creo un componente boton

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,11 @@
+import Boton from "@/components/boton/Boton"
+
 export default function Home() {
   return (
-    <div className="bg-tertiaryColor">
-      <p>home</p>
+    <div className="bg-cuartiaryColor h-full flex justify-center">
+      <div className="bg-white w-11/12 mt-4 rounded-t-2xl p-8 drop-shadow-2xl border-2">
+        <p>home</p>
+      </div>
     </div>
   );
 }
---
+++ front/src/components/boton/Boton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface BotonProps {
+  text: string;
+  link: string;
+}
+
+const Boton: React.FC<BotonProps> = ({ text, link }) => {
+  return (
+    <Link href={`/${link}`}>
+      <p className='bg-tertiaryColor text-cuartiaryColor py-2 px-8 w-32 flex justify-center rounded-full capitalize'>{text}</p>
+    </Link>
+  );
+};
+
+export default Boton;
