7c88c76b4482caf447009a8a2efa848d747b10f3
Christian Rugna <alexcr87@live.com.ar>
2024-09-18 11:19:03 -0300

    agregado swager

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('Votaciones')
+  .setDescription('Esta es una API construida con Nest para ser empleada en el modulo 5')
+  .setVersion('2.0')
+  .addBearerAuth()
+  .build()
+  const document =SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, document)
   await app.listen(3000);
 }
 bootstrap();
