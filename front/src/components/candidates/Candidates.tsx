2c4f080b59757a4d613a94027ce510d7be20266b
eiris elias <eiriselias@gmail.com>
2024-09-20 22:36:17 -0500

    conexion con back para obtener usuarios y candidatos

--- front/src/components/candidates/Candidates.tsx
+++ front/src/components/candidates/Candidates.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Cart from "@/components/cart/Cart";
 import ICandidate from "@/interfaces/ICandidate";
-import candidates from "@/helpers/candidates";
+import { getCandidates } from '@/helpers/candidate.helper';
 
-const Candidates = () => {
-  const candidate: ICandidate[] = candidates
+const Candidates = async () => {
+  const candidate: ICandidate[] = await getCandidates()
   return (
     <div>
       <div className='flex flex-col items-center'>
         <h1 className='text-3xl my-4'>Votations Candidates</h1>
         <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aperiam explicabo, possimus voluptatum est impedit labore amet dolorum cumque eligendi perspiciatis iure? Aliquam 
           magni vel ducimus atque itaque beatae molestias.</p>
+         
       </div>
       <div className='grid grid-cols-5'>
         {
--- front/src/components/cart/Cart.tsx
+++ front/src/components/cart/Cart.tsx
@@ -2,30 +2,31 @@ import React from 'react';
 import Image from 'next/image';
 import ICandidate from '@/interfaces/ICandidate';
 import IUser from '@/interfaces/IUser';
-import usersR from "@/helpers/users"
+import { getUsers } from '@/helpers/user.helper';
 
 
-const Cart: React.FC<ICandidate> = (props) => {
-  const users: IUser[] = usersR
+const Cart: React.FC<ICandidate> = async (props) => {
+  const users: IUser[] = await getUsers()
+
   return (
     <div className='bg-cuartiaryColor w-52 h-60 relative rounded-xl overflow-hidden drop-shadow-2xl border-2'>
       {
-        users.map((user)=>{
-          if(user.id === props.id){
-            return(
-            <>
-              <div className='absolute top-0 '>
-                <Image src="/images/busto.png" alt="imagenBusto" width={200} height={200}/>
+        users && users.map((user)=>{
+          return(
+            <div>
+              <div>
+                <Image src="/images/busto.png" alt="fotoCandidato" width={200} height={200}/>
               </div>
               <div className='bg-primaryColor py-4 absolute bottom-0 w-full flex flex-col items-center'>
                 <h3 className='font-bold text-xl capitalize'>{user.name}</h3>
                 <p>{props.list}</p>
               </div>
-            </>
-          )
-          }
+
+            </div>
+          )   
         })
       }
+    
     </div>
   )
 }
