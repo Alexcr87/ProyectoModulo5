7e699919f49aea8322b2a922608a33612c5c51f0
Christian Rugna <alexcr87@live.com.ar>
2024-09-22 02:44:18 -0300

    rolesGuard

--- back/src/Guards/roles.guard.ts
+++ back/src/Guards/roles.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
+import { Role } from "src/entities/roles.entity";
 
 
 
@@ -8,22 +9,34 @@ export class RolesGuard implements CanActivate {
     constructor(private readonly reflector: Reflector) {}
 
     canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest();
-        const user = request.user;
-    
-    
-        const allowedUserIds = this.reflector.get<number[]>('allowedUserIds', context.getHandler()) || [];
+    // Obtén los roles permitidos de los decoradores
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+        context.getHandler(),
+        context.getClass()
+    ]);
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+        return true; // Si no hay roles definidos, permite el acceso
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user || !user.roles) {
+        throw new ForbiddenException("Acceso no autorizado. No hay información de usuario o roles.");
         
-        if (!user || !allowedUserIds.includes(user.id)) {
-            throw new ForbiddenException("You don't have permission to access this route");
-        }
+      }
+
+    if (!user) {
+        throw new ForbiddenException("No tienes permiso para acceder a esta ruta");
+    }
+    const hasRole = user.roles && user.roles.some(role => requiredRoles.includes(role));
     
-        const hasRole = user.roles && user.roles.some(role => allowedUserIds.includes(role));
-        
-        if (!hasRole) {
-            throw new ForbiddenException("You don't have access to this route");
-        }
-    
-        return true;
+
+   if (!hasRole) {
+        throw new ForbiddenException("No tienes acceso a esta ruta");
+    }
+
+    return true;
 }
 }
