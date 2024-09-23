ef9312d0c493ea29440efc5c75ec18e6a289bad6
Christian Rugna <alexcr87@live.com.ar>
2024-09-23 17:11:14 -0300

    aboutus

---
+++ front/src/app/aboutus/page.tsx
@@ -0,0 +1,10 @@
+import AboutUs from "@/components/AboutUs/aboutUs";
+
+const AboutUsPage=()=>{
+    return(
+      <div className="bg-cuartiaryColor h-full flex justify-center">
+      <AboutUs/>
+     </div>
+    )
+};
+export default AboutUsPage
