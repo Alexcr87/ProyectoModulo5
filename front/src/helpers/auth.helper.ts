56cc1dfca2358da1efb0574a0d28975a11e4266d
eiris elias <eiriselias@gmail.com>
2024-09-23 12:08:29 -0500

    se genera el post a la api para subir la localstorage el usuario logueado

--- front/src/helpers/auth.helper.ts
+++ front/src/helpers/auth.helper.ts
@@ -1,55 +1,80 @@
 import { IRegisterProps } from "@/components/Register/TypesRegister";
+import { IloginProps } from "@/interfaces/ILogin";
 
 const APIURL: string | undefined = process.env.NEXT_PUBLIC_API_URL;
 
 export async function register(userData: IRegisterProps) {
-    try {
-      const res = await fetch(`${APIURL}/auth/sigUp`, {
-          method:"POST",
+  try {
+    const res = await fetch(`${APIURL}/auth/sigUp`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    if (!res.ok) {
+      const errorData = await res.json(); // Obtener los detalles del error
+      throw new Error(
+        `Error ${res.status}: ${errorData.message || "Failed to register"}`
+      );
+    }
+    return await res.json();
+  } catch (error: any) {
+    console.error("Registration error:", error.message);
+    throw new Error(
+      error.message || `Unknown error occurred during registration.`
+    );
+  }
+}
+
+export async function importUser(file: File) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await fetch(`${APIURL}/user/import`, {
+      method: "POST",
+      body: formData,
+      headers: {
+        // No incluyas el encabezado 'Content-Type'
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
+    // Verifica si la respuesta tiene contenido antes de intentar parsearla
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      return await response.json();
+    } else {
+      throw new Error("Respuesta no es JSON");
+    }
+  } catch (error) {
+    console.error("File upload error:", error);
+    throw error;
+  }
+}
+
+export async function login (userData: IloginProps){
+  try{
+      
+      const res = await fetch(`${APIURL}/auth/sigIn`,{
+          method: "POST",
           headers:{
               "Content-type": "application/json"
           },
           body: JSON.stringify(userData)
-           });
-           if (!res.ok) {
-            const errorData = await res.json(); // Obtener los detalles del error
-            throw new Error(`Error ${res.status}: ${errorData.message || "Failed to register"}`);
-          }
-         return await res.json();
-        } catch (error: any) {
-          console.error("Registration error:", error.message);
-          throw new Error(error.message || `Unknown error occurred during registration.`);
-        }
+      })
+      if(res.ok){
+          return res.json()
+      }else{
+          throw Error("usuario o contraseña no coincide con usuario registrado")
       }
 
-      export async function importUser (file: File) {
-        const formData = new FormData();
-        formData.append('file', file);
-      
-        try {
-          const response = await fetch('http://localhost:3000/user/import', {
-            method: 'POST',
-            body: formData,
-            headers: {
-              // No incluyas el encabezado 'Content-Type'
-            },
-          });
-      
-          if (!response.ok) {
-            throw new Error(`Error ${response.status}: ${response.statusText}`);
-          }
-      
-          // Verifica si la respuesta tiene contenido antes de intentar parsearla
-          const contentType = response.headers.get("content-type");
-          if (contentType && contentType.includes("application/json")) {
-            return await response.json();
-          } else {
-            throw new Error("Respuesta no es JSON");
-          }
-      
-        } catch (error) {
-          console.error('File upload error:', error);
-          throw error;
-        }
-      };
-      
+  } catch (error: any){
+      alert("Usuario o contraseña no coincide con usuario registrado")
+  }
+  
+}
