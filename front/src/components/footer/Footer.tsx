86e4edec70354d93ca4bdda1ecf32b47cff8d3a4
Christian Rugna <alexcr87@live.com.ar>
2024-09-24 11:43:57 -0300

    footer modificado

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = () => {
   return (
