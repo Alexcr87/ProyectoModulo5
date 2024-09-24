db1319b616c7bd8edcec3c5b4694011a8910fa6f
Christian Rugna <alexcr87@live.com.ar>
2024-09-24 11:52:14 -0300

    GUIA MODIFICADA

---
+++ front/src/app/guia/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from 'react';
+
+const guia=()=>{
+return(
+  <div className="relative h-screen w-full bg-cover bg-center" style={{ backgroundImage: "url('https://img.freepik.com/premium-zdjecie/wyborczyni-wkladajaca-kartke-do-urny-wyborczej-wybory-i-koncepcja-glosowania_77190-18358.jpg')" }}>
+  {/* Overlay para oscurecer la imagen y mejorar la legibilidad del texto */}
+  <div className="absolute inset-0 bg-black opacity-50"></div>
+
+  <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white p-4">
+    <h1 className="text-4xl font-bold mb-6">
+      PROXIMAMENTE
+    </h1>
+  </div>
+  <style jsx>{`
+    .bg-primary-color {
+      background-color: #F45D01;
+    }
+  `}</style>
+</div>
+)
+}
+
+export default guia
+
+
