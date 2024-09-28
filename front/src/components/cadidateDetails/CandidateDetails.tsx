ffcdd5195798eca1e82a3a83ff7476814800d681
eiris elias <eiriselias@gmail.com>
2024-09-28 11:45:05 -0500

    modificacion de candidate details

--- front/src/components/cadidateDetails/CandidateDetails.tsx
+++ front/src/components/cadidateDetails/CandidateDetails.tsx
@@ -3,9 +3,19 @@ import React from 'react'
 import ICandidate from '@/interfaces/ICandidate'
 import Image from 'next/image'
 import Swal from 'sweetalert2'
+import Boton2 from '../ui/Boton2'
 
 
 const CandidateDetails = async (props:ICandidate) => {
+  
+  const convertirArreglo = () =>{
+    const variable: string[] | undefined =props.proposals
+
+  const resultArray: string[] = typeof variable === 'string' ? JSON.parse(variable) : [];
+
+      return resultArray; 
+  }
+
     const handleVotar = ()=>{
       Swal.fire({
         position: "center",
@@ -19,7 +29,7 @@ const CandidateDetails = async (props:ICandidate) => {
   return (
     <div className='w-full flex flex-col items-center'>
       <div className='bg-white w-11/12 p-4 flex flex-col items-center rounded-t-2xl drop-shadow-2xl border-2 mt-4'>
-        <h1 className='text-2xl font-bold capitalize text-tertiaryColor'>Candidato a Votacion {props.postulation}</h1>
+        <h1 className='text-2xl font-bold capitalize'>Candidato a Votacion {props.postulation}</h1>
         <p>{props.campaignDescription}</p>
       </div>
       <div className='grid grid-cols-2 w-11/12 justify-center gap-2 h-[65vh]'>
@@ -29,19 +39,24 @@ const CandidateDetails = async (props:ICandidate) => {
                     <Image src={props.imgUrl} alt="fotoCandidato" fill />
                 </div>
                 <div className='bg-primaryColor py-4 absolute bottom-0 w-full flex flex-col items-center'>
-                    <h3 className='font-bold text-xl capitalize'>{props.user.name}</h3>
-                    <p>{props.list}</p>
-                    <button
-                    className='bg-tertiaryColor text-cuartiaryColor rounded-full px-12 py-2 hover:scale-105 hover:bg-secundaryColor ease-in-out duration-300 hover:text-tertiaryColor'
-                    onClick={handleVotar}
-                    >Votar</button>
+                    <h3 className='font-bold text-xl capitalize text-white'>{props.user.name}</h3>
+                    <p className='text-white mb-2'>{props.list}</p>
+                    <div>
+                      <Boton2 onClick={handleVotar}>Votar</Boton2>
+                    </div>
                 </div>
             </div>
         </div>
         <div className='bg-white mt-2 drop-shadow-2xl border-2 p-4 rounded-b-2xl'>
             <div className='bg-secundaryColor h-full p-4 rounded-2xl drop-shadow-2xl text-cuartiaryColor'>
                 <h2>Propuestas:</h2>
-                <p className='ml-4'>{props.proposals}</p>   
+                {
+                  convertirArreglo().map((propu, index)=>{
+                    return(
+                      <p className='ml-4' key={index}>{`${index + 1}. ${propu}`}</p>   
+                    )
+                  })
+                }
             </div>
         </div>
       </div>
