e27f661d6a4e171926a33d796c746659bd21d9db
nmainer <mainermnicolas@gmail.com>
2024-09-28 18:18:31 -0300

    ApiTags Payments

--- back/src/modules/payments/payment.controller.ts
+++ back/src/modules/payments/payment.controller.ts
@@ -4,8 +4,11 @@ import { Repository } from "typeorm";
 import { Account } from "src/entities/account.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { PaymentDto } from "src/dto/payment.dto";
+import { ApiTags } from "@nestjs/swagger";
 
 
+
+@ApiTags("Payments")
 @Controller('payments')
 export class PaymentsController {
     constructor( private readonly paymentService:PaymentsService , @InjectRepository(Account) private readonly accountRepository: Repository<Account>){}
@@ -22,7 +25,7 @@ export class PaymentsController {
   
  
     @Post('create')
-    async createPayment(@Body() PaymentDto) {
+    async createPayment(@Body() PaymentDto:PaymentDto) {
       const {userId ,packageId, payerEmail , paymentMethodId, installments} = PaymentDto;
       return this.paymentService.createPayment(userId ,packageId, payerEmail , paymentMethodId,installments);
     }
