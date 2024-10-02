7737e6ad32978dce2235a03bd951a9b67455a927
Christian Rugna <alexcr87@live.com.ar>
2024-10-02 18:52:02 -0300

    uniendo estilos con lo de MP ya separadas las paginas

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,14 +2,16 @@ import Inicial from '@/components/inicial/inicial';
 import Inicial2 from '@/components/inicial/inicial2';
 import Inicial3 from '@/components/inicial/inicial3';
 import PricingTable from '@/components/pricingTable/PricingTable';
+import PricingTable2 from '@/components/pricingTable/PricingTable2';
+import PricingTable3 from '@/components/pricingTable/PricingTable3';
 import TeamMembers from '@/components/teamMembers/TeamMembers';
 import React from 'react';
 
 export default function Home() {
   return (
     <div>
-      <Inicial3/>
-       {/* <PricingTable3/>  */}
+      <Inicial/>
+        <PricingTable3/> 
         <TeamMembers/> 
     </div>
   );
