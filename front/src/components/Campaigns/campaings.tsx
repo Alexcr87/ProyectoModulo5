1f74944fe3d7d49b8e5960f2ef5ff56a711db553
Ramon Gonzalez <ramongonzalez101@gmail.com>
2024-10-08 11:10:28 -0400

    seleccion campana, ir a actualizacion campana

--- front/src/components/Campaigns/campaings.tsx
+++ front/src/components/Campaigns/campaings.tsx
@@ -15,6 +15,7 @@ const CampaignsTable = () => {
     const [groups, setGroups] = useState<string[]>([])
     const pathname = usePathname();
     const router = useRouter()
+    const [selectedCampaigns, setSelectedCampaigns] = useState<ICampaign[]>([]);
 
     useEffect(() => {
         if (userData) {
@@ -75,33 +76,55 @@ const CampaignsTable = () => {
         }
     }
 
+    const handleSelect = (checked: boolean, campaign: ICampaign) => {
+        if (checked) {
+            setSelectedCampaigns([...selectedCampaigns, campaign]);
+        } else {
+            setSelectedCampaigns(selectedCampaigns.filter((c) => c.id !== campaign.id));
+        }
+    };
+
+    // esta funcion entiendo que deberia integrarse en handleAction y depender de los roles
+    const handleUpdate = (id: string | undefined ) => {
+        router.push(`/updateCampaing?id=${id}`);
+      };
+
     return (
         <div className="mt-4 overflow-x-auto">
             <h1 className="text-2xl font-bold mb-4 text-center">Mis Campañas</h1>
+            <div className="mb-4">
+        <button className='bg-primaryColor  text-cuartiaryColor py-2 px-4 flex justify-center rounded-lg hover:scale-105 hover:bg-primaryColor duration-300'>
+            Eliminar seleccionados
+        </button>
+            </div>
             {campaigns.length > 0 ? (   
                 <table className="min-w-full border-collapse">
                     <thead>
                         <tr className="bg-primaryColor text-left text-white">
+                        <th className="border p-2">Seleccionar</th>
                             <th className="border p-2">Nombre</th>
                             <th className="border p-2">Descripción</th>
                             <th className="border p-2">Ubicación</th>
                             <th className="border p-2">Fecha</th>
-                            <th className="border p-2">Accion</th>
+                            <th className="border p-2">Ver</th>
+                            <th className="border p-1">Actualizar</th>
                         </tr>
                     </thead>
                     <tbody>
                         {campaigns.map((campaign, index) => (
                             <tr key={index} 
                                 className={`${index % 2 === 0 ? "bg-gray-50" : "bg-white"} border-t border-gray-200`}
-                                onClick={()=>handleAction(campaign.id)}
+                                // onClick={()=>handleAction(campaign.id)}
                             >
+                                <td className="border p-2"><input type="checkbox" onChange={(e) => handleSelect(e.target.checked,campaign)}/></td>
                                 <td className="border p-2">{campaign.name}</td>
                                 <td className="border p-2">{campaign.description}</td>
                                 <td className="border p-2">{campaign.location}</td>
                                 <td className="border p-2">{new Date(campaign.date).toLocaleDateString()}</td>
-                                <td className="border p-2 text-blue-500 hover:text-blue-700 ">
+                                <td className="border p-2 text-blue-500 hover:text-blue-700 cursor-pointer " onClick={()=>handleAction(campaign.id)}>
                                     {roles.includes('candidate') || roles.includes('voter') ? 'votar' : 'ver'}
                                 </td>
+                                <td className="border p-1 text-blue-500 hover:text-blue-700 cursor-pointer" onClick={() => handleUpdate(campaign.id)}>Actualizar </td>
                             </tr>
                         ))}
                     </tbody>
