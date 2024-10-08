35d979fdedc7f06c0f32aad005b54dcd460c3f0b
Christian Rugna <alexcr87@live.com.ar>
2024-10-08 11:48:26 -0300

    modal

--- front/src/components/inicial/inicial.tsx
+++ front/src/components/inicial/inicial.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Modal from "react-modal";
+import Modal from 'react-modal'
 import React, { useEffect, useState } from 'react';
 import CustomModal from '../InstructionsModal/Modal'; 
 
