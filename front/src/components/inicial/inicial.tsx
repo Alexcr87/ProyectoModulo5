648c363ae8362c1162a78f41527816e9ea31068e
eiris elias <eiriselias@gmail.com>
2024-10-14 13:26:05 -0500

    Se agrego nombre de usuario ingresado en la pagina principal

--- front/src/components/inicial/inicial.tsx
+++ front/src/components/inicial/inicial.tsx
@@ -44,7 +44,11 @@ const Inicial = () => {
         <div className="relative w-full bg-cover bg-center h-[90vh]" style={{ backgroundImage: "url('https://img.freepik.com/premium-zdjecie/wyborczyni-wkladajaca-kartke-do-urny-wyborczej-wybory-i-koncepcja-glosowania_77190-18358.jpg')" }}>
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white p-4">
-                <h1 className="text-4xl font-bold mb-6">Bienvenido a la Plataforma de Votación</h1>
+                {
+                    userData ? (<h1 className="text-4xl font-bold mb-6">Bienvenido <span className='capitalize'>{userData.userData.name}</span> a la Plataforma de Votación</h1>) 
+                    :( <h1 className="text-4xl font-bold mb-6">Bienvenido a la Plataforma de Votación</h1> )
+                }
+                
                 <p className="text-lg mb-2">Nuestra plataforma ofrece una experiencia intuitiva y accesible para que puedas gestionar elecciones de manera eficiente.</p>
                 <button 
                     onClick={openModal} 
