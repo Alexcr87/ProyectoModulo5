742b9d30cd34a6c89d3f712415e16b6cbfb8cc23
Ramon Gonzalez <ramongonzalez101@gmail.com>
2024-10-14 14:21:32 -0400

    crear popupselect register y modificar la ruta de registers

--- front/src/app/registerUsers/page.tsx
+++ front/src/app/registerUsers/page.tsx
@@ -1,13 +1,16 @@
 // app/prueba/page.tsx
 import PruebaPageComponent from '@/components/popUpRegister/popUpRegister';
+import PopUPSelectRegister from '@/components/popUpSelectRegister/popUpSelectRegister';
 import React from 'react';
 
 
 const PruebaPage = () => {
   return (
     <div>
-     <h1>PopUp para el registro</h1>
-      <PruebaPageComponent />
+     {/* <h1>PopUp para el registro</h1>
+      <PruebaPageComponent /> */}
+      <PopUPSelectRegister/>
+
     </div>
   );
 };
