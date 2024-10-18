c372fa8145ebe26bcda07c564b1528932322f72e
Christian Rugna <alexcr87@live.com.ar>
2024-10-18 00:13:21 -0300

    precarga de updateCampaign

--- front/src/components/updateCampaign/updateCampaign.tsx
+++ front/src/components/updateCampaign/updateCampaign.tsx
@@ -46,6 +46,21 @@ const updateCampaign = () => {
         .then(response => response.json())
         .then(data => {
           setCampaign(data);
+          setFormData({
+            name: data.name,
+            description: data.description,
+            location: data.location,
+            date: new Date(data.date), // Asegúrate de que la fecha se maneje correctamente
+            userId: userData?.userData.id || '', 
+            user: { 
+              id: userData?.userData.id || '', 
+              name: userData?.userData.name || '', 
+              dni: 0, 
+              email: '' 
+            },
+            candidates: [],
+            groups: [] 
+          });
           setLoading(false);
         })
         .catch(error => {
@@ -114,13 +129,14 @@ const updateCampaign = () => {
         
         <div className="flex flex-col md:flex-row items-center w-full md:w-4/5 lg:w-3/5 gap-4 m-auto mt-8 py-8 px-6 font-bold border-4 rounded-lg border-stroke dark:border-dark-3 border-primaryColor shadow-lg">
           <p>{`Campana: ${campaign?.name}`}</p>
-          <p>{`Fecha: ${campaign?.date ? new Intl.DateTimeFormat('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric', timeZone: 'UTC'  }).format(new Date(campaign.date)) : ''}`}</p>
+          <p>{`Fecha: ${campaign?.date ? new Date(campaign.date).toLocaleDateString().split('T')[0] : ''}`}</p>
         </div>
       </div>
       <form className="campaign-form flex justify-center" onSubmit={handleUpdateCampaign}>
         <div className="flex flex-col items-center w-full md:w-[40%] gap-4">
           {/* Nombre */}
           <div className="relative w-full">
+            
             <label className="flex items-center">
               Nombre
               <span className="ml-2 text-primaryColor cursor-pointer" data-tooltip-id="tooltip-name">
@@ -137,6 +153,8 @@ const updateCampaign = () => {
               className="w-full px-5 py-3 text-base transition bg-transparent border rounded-md outline-none 
                 border-stroke dark:border-dark-3 text-body-color dark:text-dark-6 placeholder:text-black focus:border-primaryColor 
                 dark:focus:border-primaryColor focus-visible:shadow-none"
+                value={formData.name}
+                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
               required
             />
           </div>
@@ -157,6 +175,8 @@ const updateCampaign = () => {
               className="w-full h-40 px-5 py-3 text-base transition bg-transparent border rounded-md outline-none 
                 border-stroke dark:border-dark-3 text-body-color dark:text-dark-6 placeholder:text-black focus:border-primaryColor 
                 dark:focus:border-primaryColor focus-visible:shadow-none"
+                value={formData.description} // Vincula el textarea al estado formData
+              onChange={(e) => setFormData({ ...formData, description: e.target.value })} // Maneja el cambio
               required
             />
           </div>
@@ -178,6 +198,8 @@ const updateCampaign = () => {
               className="w-full px-5 py-3 text-base transition bg-transparent border rounded-md outline-none 
                 border-stroke dark:border-dark-3 text-body-color dark:text-dark-6 placeholder:text-black focus:border-primaryColor 
                 dark:focus:border-primaryColor focus-visible:shadow-none"
+                value={formData.location} // Vincula el input al estado formData
+                onChange={(e) => setFormData({ ...formData, location: e.target.value })} // Maneja el cambio  
               required
             />
           </div>
